18b400cf606aa42bd2ccf5901214cc774410e2c8
Wolf Noble <wnoble@datapipe.com>
2013-05-06 18:17:31 -0500

    AUT-202 refactor mysql class to use puppetlabs-mysql vanilla. Create new namespace class

---
+++ Modulefile
@@ -0,0 +1,12 @@
+name    'datapipe-mysqlx'
+version '0.0.1'
+source 'UNKNOWN'
+author 'datapipe'
+license 'Apache License, Version 2.0'
+summary 'MySQL Wrapper Class'
+description 'Generic helper class module'
+project_page 'UNKNOWN'
+
+## Add dependencies, if any:
+# dependency 'username/name', '>= 1.2.0'
+dependency 'puppetlabs/mysql', '>= 0.6.2'
---
+++ manifests/init.pp
@@ -0,0 +1,23 @@
+# == Class: mysqlx
+#
+# Full description of class mysqlx here.
+#
+# === Parameters
+#
+# === Variables
+#
+# === Examples
+#
+# This class shouldn't ever be invoked directly. It simply is a namespace
+#
+# === Authors
+#
+# Wolf Noble <wnoble@datapipe.com>
+#
+# === Copyright
+#
+# Copyright 2013 Datapipe, unless otherwise noted.
+#
+class mysqlx {
+  notice "The mysqlx class should not be decalred directly. Please refer to the documentation."
+}
